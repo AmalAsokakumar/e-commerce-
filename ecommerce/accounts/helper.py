9bedc22f6eca0b552be431ab8d32d9e19c51e65f
amal <cena3799@gmail.com>
2022-10-17 13:56:55 +0530

    first commit

--- ecommerce/accounts/helper.py
+++ ecommerce/accounts/helper.py
@@ -1,4 +1,5 @@
 from twilio.rest import Client
+
 # for environ variable
 from ecommerce.settings import env
 
@@ -7,25 +8,25 @@ from ecommerce.settings import env
 # and set the environment variables. See http://twil.io/secure
 def sent_otp(mobile):
     phone = "+91" + str(mobile)
-    account_sid = env('account_sid')
-    auth_token = env('auth_token')
+    account_sid = env("account_sid")
+    auth_token = env("auth_token")
     client = Client(account_sid, auth_token)
 
-    verification = client.verify.services(
-        env('V_ID')
-    ).verifications.create(to=phone, channel="sms")
+    verification = client.verify.services(env("V_ID")).verifications.create(
+        to=phone, channel="sms"
+    )
 
     print(verification.status)
 
 
 def check_otp(mobile, otp):
-    account_sid = env('account_sid')
-    auth_token = env('auth_token')
+    account_sid = env("account_sid")
+    auth_token = env("auth_token")
     client = Client(account_sid, auth_token)
 
-    verification_check = client.verify.services(
-        env('V_ID')
-    ).verification_checks.create(to="+91" + mobile, code=otp)
+    verification_check = client.verify.services(env("V_ID")).verification_checks.create(
+        to="+91" + mobile, code=otp
+    )
 
     print(verification_check.status)
     if verification_check.status == "approved":
